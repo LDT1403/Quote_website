28a11ea6040cf3c75c1da5c8d154e2d93aee3547
LE DINH THANH <thanhldse160337@fpt.edu.vn>
2024-03-11 14:21:05 +0700

    staff status

--- Quote/Controllers/TaskController.cs
+++ Quote/Controllers/TaskController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Quote.Interfaces.ServiceInterface;
+using Quote.Modal;
 
 namespace Quote.Controllers
 {
@@ -9,6 +10,7 @@ namespace Quote.Controllers
     public class TaskController : ControllerBase
     {
         private readonly ITaskInterface _taskService;
+        private readonly IRequestService _requestService;
 
         public TaskController(ITaskInterface taskService)
         {
@@ -28,6 +30,33 @@ namespace Quote.Controllers
             }catch (Exception ex)
             {
                 return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPost("CreateTask")]
+        public async Task<IActionResult> CreateTask(int requestId, int staffId, [FromBody]TaskReq task)
+        {
+            try
+            {
+                var request = await _requestService.Appoinment(requestId);
+                
+                if(request != null)
+                {
+                    Models.Task newTask = new Models.Task();
+                    newTask.RequestId = request.RequestId; ;
+                    newTask.UserId = staffId;
+                    newTask.TaskName = task.TaskName;
+                    newTask.Status = "0";
+                    newTask.Location = request.Address;
+                    var item = await _taskService.CreateTasks(newTask);
+                    return Ok("Success");
+                }
+                return NotFound();
+              
+
+            }catch (Exception ex)
+            {
+                return BadRequest($"{ex.Message}");
             }
         }
     }
