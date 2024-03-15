a48cb98ecd25359a44b97105227cb2b8240eb313
HoangChiDuong <duonghoangasd@gmail.com>
2024-03-15 14:42:02 +0700

    ApiAdmin

--- Quote/Modal/ContratAdminResponse.cs
+++ Quote/Modal/ContratAdminResponse.cs
@@ -3,4 +3,9 @@
     public class ContratAdminResponse
     {
     }
+    public class ContractAdmin
+    {
+        public int  contractId { get; set; }
+        public byte[] contractfile { get; set; }
+    }
 }
